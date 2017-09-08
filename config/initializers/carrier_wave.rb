c1f507dc46271458ff01e471a6710a8e3444e9f8
rvillasevil <rvillasevil@gmail.com>
2017-09-08 23:11:00 +0200

    change aws intead s3 in carrierwase keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
        #Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :region 				 =>	'eu-west-2'
     }
     config.fog_directory     =  ENV['S3_BUCKET']
