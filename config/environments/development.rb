11ffbd9a063a9ab860f8d38a1cb7c6da767281e5
rvillasevil <rvillasevil@gmail.com>
2017-09-10 17:00:22 +0200

    deploy sucessfully

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,16 +30,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   #host = 'localhost:3000'
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "rvillasevil@gmail.com"
-    password: "taniamylove"
+    #address: "smtp.gmail.com",
+    #port: 587,
+    #domain: 
+    #authentication: "plain",
+    #enable_starttls_auto: true,
+    #user_name: "rvillasevil@gmail.com",
+    #password: "taniamylove",
   }
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  #config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
