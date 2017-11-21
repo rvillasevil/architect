3167fe81d16b18319cdcea865aec467d6ee09ba6
rvillasevil <rvillasevil@gmail.com>
2017-11-21 23:18:30 +0100

    211117_1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
 	belongs_to :user, required: false
 	belongs_to :micropost, required: false
 
-	validates :contenido, presence: true, length: { maximum: 450 }
+	validates :contenido, presence: true, length: { maximum: 1000 }
 
 end
