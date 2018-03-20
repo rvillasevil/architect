398df59ac5f4dd424fb859e32921a29ca9f711bf
rvillasevil <rvillasevil@gmail.com>
2018-03-20 23:45:02 +0100

    changes in productos https from email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'constructux.com'
-  config.action_mailer.default_url_options = { host: 'constructux.com' }
+  host = 'https://www.constructux.com'
+  config.action_mailer.default_url_options = { host: 'https://www.constructux.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           => '587',
