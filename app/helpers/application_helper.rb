219dc225b10b1ebd6acdea64908f7b61cf404f5a
rvillasevil <rvillasevil@gmail.com>
2018-03-22 14:09:39 +0100

    220218 after google SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "ConstructUX, donde diseñar tu reforma o construir tu vivienda."
+    base_title = "ConstructUX, donde diseñar el presupuesto de tu reforma o construir tu vivienda a medida."
     if page_title.empty?
       base_title
     else
